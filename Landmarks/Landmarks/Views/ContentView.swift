8a7277ceccaf12876a3d0a15ae5fdf6bf157779a
Juan Francisco Bazan Carrizo <juanfrancisco.bazancarrizo@mercadolibre.com>
2022-05-20 11:37:50 -0300

    building-lists-and-navigation

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Juan Francisco Bazan Carrizo on 25/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
